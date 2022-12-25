335482a986c6274a6ce6cfb07ca2c447b6f16572
juanito87 <juancruz.f87@gmail.com>
2022-12-25 01:31:20 -0300

    Adding remap to git commit

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,3 +1,5 @@
 -- set git status remap
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
---vim.keymap.set("n", "<leader>gc", vim.cmd.Git('commit')) fix trigger only when leadergc is called
+vim.keymap.set("n", "<leader>gc", function()
+    vim.cmd.Git('commit')
+end) 
