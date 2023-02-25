3f47b4f45c579e155003d5b078333d08eebdffa3
juanito87 <juancruz.f87@gmail.com>
2023-02-24 22:40:12 -0300

    Updating config

--- lua/juanito/init.lua
+++ lua/juanito/init.lua
@@ -16,6 +16,3 @@ vim.g.netrw_altv = 1                  -- Files are opened to the right of netrw
 vim.g.netrw_chgwin = -1               -- Files are opened in the netrw window
 vim.g.netrw_winsize = 25              -- Window size
 vim.g.netrw_list_hide= ".*.swp$, *.pyc$,  *.log$,  *.o$,  *.xmi$,  *.swp$,  *.bak$,  *.pyc$,  *.class$,  *.jar$,  *.war$,  *__pycache__*" -- Hide files with this extensions
---vim.g.netrw_browse_split = 0          -- Open netrw reusing the window| not good combination with pv.
---vim.g.netrw_browse_split = 0          -- New files are opened in a new vertical
---vim.g.netrw_sort_sequence = [\/]$,* -- Default config
