946b2b02c138893a033c718f92c2f54973bde7af
juanito87 <juancruz.f87@gmail.com>
2024-04-21 13:00:49 -0300

    Updating packer config

--- lua/juanito/packer.lua
+++ lua/juanito/packer.lua
@@ -4,34 +4,6 @@ return require('packer').startup(function(use)
 -- Packer can manage itself
 use 'wbthomason/packer.nvim'
 
--- fuzzy finder
-use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-  'nvim-lua/popup.nvim',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
-use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-
--- Color scheme
-use { "ellisonleao/gruvbox.nvim" }
-
--- highligh, syntax, comments, etc
-use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-use ('numToStr/Comment.nvim') -- comments
-use ('mbbill/undotree') -- undo tree
-
--- improve workflow for multiple files
--- mark files for quick access menu
-use('theprimeagen/harpoon')
-
--- git management
-use('tpope/vim-fugitive')
-use('tpope/vim-sleuth')
-use('APZelos/blamer.nvim')
-use('ThePrimeagen/git-worktree.nvim')
--- Commenting out copilot, not usefull enough to pay 10 a month.
--- use('github/copilot.vim')
-
 -- lsp (language server protocol)
 use {
   'VonHeikemen/lsp-zero.nvim',
@@ -54,6 +26,37 @@ use {
     {'rafamadriz/friendly-snippets'},
   }
 }
+
+-- fuzzy finder
+use {
+  'nvim-telescope/telescope.nvim',
+  'nvim-lua/popup.nvim',
+  requires = { {'nvim-lua/plenary.nvim'} }
+}
+use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+use ('nvim-telescope/telescope-file-browser.nvim')
+-- Color scheme
+use { "ellisonleao/gruvbox.nvim" }
+
+-- improve workflow for multiple files
+-- mark files for quick access menu
+use('theprimeagen/harpoon')
+
+-- git management
+use('tpope/vim-fugitive')
+use('tpope/vim-sleuth')
+use('APZelos/blamer.nvim')
+use('ThePrimeagen/git-worktree.nvim')
+-- Commenting out copilot, not usefull enough to pay 10 a month.
+-- use('github/copilot.vim')
+
+-- highligh, syntax, comments, etc
+use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+use ('numToStr/Comment.nvim') -- comments
+use ('mbbill/undotree') -- undo tree
+
+-- -- filetype management
+-- -- use("nathom/filetype.nvim")
 
 -- markdown visualizer
 use {"ellisonleao/glow.nvim", config = function() require("glow").setup() end}
