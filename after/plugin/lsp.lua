dba2b899acec692c0df42789534676d8c2a88d15
juanito87 <juancruz.f87@gmail.com>
2024-04-21 12:58:06 -0300

    Adding config to on buffer attach

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -51,11 +51,31 @@ lsp.set_preferences({
     }
 })
 
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
+
 lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
+})
 
 vim.api.nvim_set_keymap('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true }) -- open a float window to check lsp message
 vim.api.nvim_set_keymap('n', '<leader>d[', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true }) -- go to previous error
 vim.api.nvim_set_keymap('n', '<leader>d]', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true }) -- go to next error
 vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true }) -- open telescope to check lsp message
--- vim.api.nvim_set_keymap('n', '<leader>gb', '<cmd>lua vim.gitblame.open_float()<CR>', { noremap = true, silent = true }) -- open a float window to check lsp message
--- vim.api.nvim_set_keymap("n", "<leader>Q", "<cmd>lua require'utils'.toggle_qf('l')<CR>", { desc = "toggle location list" }) -- show remaps on quickfix
+vim.api.nvim_set_keymap('n', '<leader>gb', '<cmd>lua vim.gitblame.open_float()<CR>', { noremap = true, silent = true }) -- open a float window to check lsp message
+vim.api.nvim_set_keymap("n", "<leader>Q", "<cmd>lua require'utils'.toggle_qf('l')<CR>", { desc = "toggle location list" }) -- show remaps on quickfix
+
